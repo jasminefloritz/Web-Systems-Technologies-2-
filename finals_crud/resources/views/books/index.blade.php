c0e21479b9e0777145b328c0e4d45b2dc89948f5
jasminefloritz <floritzj@gmail.com>
2026-04-22 16:00:14 +0800

    Activity-Finals:Using ORM

---
+++ finals_crud/resources/views/books/index.blade.php
@@ -0,0 +1,32 @@
+<h1>All Books</h1>
+
+<h2>Activity-Finals: Using ORM</h2>
+<h3>Floritz Jasmine S. Dumpit</h3>
+<a href="{{ route('books.create') }}">Add New Book</a>
+
+<ul>
+
+@foreach ($books as $book)
+
+<li>
+
+{{ $book->title }} by {{ $book->author }} ({{ $book->published_date }})
+
+<a href="{{ route('books.edit', $book->id) }}">Edit</a>
+
+<form action="{{ route('books.destroy', $book->id) }}" method="POST" style="display:inline;">
+
+@csrf
+
+@method('DELETE')
+
+<button type="submit">Delete</button>
+
+</form>
+
+</li>
+
+@endforeach
+
+</ul>
+
